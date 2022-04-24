e77fdabee742881b1389e36d184291160054621a
Tyler Holhubner <tholhubner@gmail.com>
2022-04-23 20:46:52 -0400

    setup App.js with all needed screens in the app. Signup and login added for now, but likely removed later.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 // local imports
-import Feed from './screens/feed'
+import Main from './screens/main'
 import Profile from "./screens/profile"
 import Login from "./screens/login"
 import Signup from "./screens/signup"
+import PostView from "./screens/postview"
 
 const CatStack = createNativeStackNavigator()
 
@@ -24,10 +25,11 @@ class App extends Component {
     return (
       <NavigationContainer>
         <CatStack.Navigator>
-          <CatStack.Screen name="Feed" component={Feed} />
+          <CatStack.Screen name="Main" component={Main} />
+          <CatStack.Screen name="Signup" component={Signup} />
           <CatStack.Screen name="Profile" component={Profile} />
           <CatStack.Screen name="Login" component={Login} />
-          <CatStack.Screen name="Signup" component={Signup} />
+          <CatStack.Screen name="PostView" component={PostView} />
         </CatStack.Navigator>
       </NavigationContainer>
     )
